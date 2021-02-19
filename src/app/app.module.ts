59729e000bbf4da0dbf8991a8fa1ce8041f47e02
mark-enriquez23 <johnednansy@gmail.com>
2021-02-19 16:50:54 +0800

    Initial updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent,
+    LoginComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
