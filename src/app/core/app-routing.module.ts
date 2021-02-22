22c53d17a702837202db6d8a15fd05a0e41186b8
mark-enriquez23 <markenriquez23.87@gmail.com>
2021-02-22 15:30:40 +0800

    landing page structure

--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'landing',
+    pathMatch: 'full'
+  },
+  {
+    path: 'landing',
+    loadChildren: () => import('src/app/modules/landing/landing.module').then( m => m.LandingModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LandingRoutingModule } from './landing-routing.module';
+
+
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    LandingRoutingModule
   ]
 })
 export class LandingModule { }
